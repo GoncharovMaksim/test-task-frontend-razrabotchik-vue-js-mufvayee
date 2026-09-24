312e982030efd321c3e9729289489d2f83cf316d
Maksim Goncharov <goncharovmaksim112@gmail.com>
2026-09-24 22:03:34 +0300

    fix: remove conflicting compiled api/index.js and api/index.d.ts

--- api/index.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from '../server/app';
-const app = createApp();
-export default function handler(req, res) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return app(req, res);
-}
